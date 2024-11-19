d96cd9fc862e9b150519576bb92559edaf2198a8
Pasindu Dilshan <dilshanp5400@gmail.com>
2024-11-19 01:17:44 -0800

    Create server, connect mongo DB and main folder structue.

--- front-end/src/Pages/Reports/Page1.jsx
+++ front-end/src/Pages/Reports/Page1.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Breadcrumb } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Page1() {
   return (
-    <div>Page1</div>
+    <div>
+        <div>
+<Breadcrumb>
+    <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>
+        JRN Roofing
+    </Breadcrumb.Item  >{"/"}
+
+    <Breadcrumb linkAs={Link} linkProps={{ to: "/report"}}>
+    Report
+    </Breadcrumb>{"/"}
+</Breadcrumb>
+
+        </div>
+
+        <p>Breadcrumbs</p>
+        </div>
   )
 }
 
